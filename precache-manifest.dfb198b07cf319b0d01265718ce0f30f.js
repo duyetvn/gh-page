4d209f58a43873bcb4e8da1bacc6717ea5457111
Duyet Nguyen <duyet.vn@gmail.com>
2021-06-20 15:29:24 +0000

    deploy

---
+++ precache-manifest.dfb198b07cf319b0d01265718ce0f30f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7f1e5c032e76862a001",
+    "url": "/hello/css/app.c39ac9c0.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/hello/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "a6ba3f0bc2b25f222699dd3082131130",
+    "url": "/hello/index.html"
+  },
+  {
+    "revision": "bdccab027fafc252018a",
+    "url": "/hello/js/about.ff32cdf2.js"
+  },
+  {
+    "revision": "e7f1e5c032e76862a001",
+    "url": "/hello/js/app.1037b9bb.js"
+  },
+  {
+    "revision": "8fa219feb823a7718b06",
+    "url": "/hello/js/chunk-vendors.2d6e9e23.js"
+  },
+  {
+    "revision": "f5a9e944b30bdafb0b7dd60a8b6f47b6",
+    "url": "/hello/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/hello/robots.txt"
+  }
+]);
